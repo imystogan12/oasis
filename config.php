6a8aefe3712a8bce7de07e4e71c4f5dd0a7694ad
Aldrin John Lopez <lopezaldrinjohn4@gmail.com>
2021-11-24 20:49:20 +0800

    push

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 	$siteUrl = 'http://localhost';
-	$oasisEmail = 'oasis.appointment.group.2021@gmail.com';
-	$oasisEmailPassword = 'Aldrino2021';
-	$awsKey = 'AKIAXD6T7WYHWPIXQ2KX';
-	$awsSecret = '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';
+	$oasisEmail = '';
+	$oasisEmailPassword = '';
+	$awsKey = '';
+	$awsSecret = '';
 ?>
